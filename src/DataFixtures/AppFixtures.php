53426b9273d7a834c1756a52ef6c0a7c4b478ba9
Lech Sawon <lech.sawon@docplanner.com>
2021-12-17 08:47:58 +0100

    doctrine fixture - faker

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,21 +5,27 @@ namespace App\DataFixtures;
 use App\Entity\Post;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $manager): void
+	public function load(ObjectManager $manager): void
     {
+		$faker = Factory::create();
+
 		$batchSize = 20;
 
 		for($i = 0; $i < 1000; $i++) {
+
+			$date = $faker->dateTime;
+
 			$post = new Post();
 			$post
-				->setTitle('title')
-				->setContent('content')
-				->setAuthor('autor')
-				->setCreatedAt(new \DateTimeImmutable())
-				->setUpdatedAt(new \DateTimeImmutable());
+				->setTitle($faker->realText(100))
+				->setContent($faker->realText())
+				->setAuthor($faker->name)
+				->setCreatedAt(new \DateTimeImmutable($date->format('c')))
+				->setUpdatedAt(new \DateTimeImmutable($date->format('c')));
 
 			$manager->persist($post);
 			if (($i % $batchSize) === 0) {
