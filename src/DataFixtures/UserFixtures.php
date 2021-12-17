dad1ce5789b5dd747821728b600040315f7b9367
Lech Sawon <lech.sawon@docplanner.com>
2021-12-17 13:50:13 +0100

    UserRegisterUseCase in fixture

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -16,6 +16,9 @@ final class UserFixtures extends Fixture
 
 	public function load(ObjectManager $manager): void
 	{
+		//używanie Serwisu z całym usecasme w FIXTURE nie jest najlepszeym pomysłem
+		//gdyż service moze mieć efekty uboczne, które nie powinny miec miejsca przy ładowaniu fixtures
+		// to jest tylko przykład reużywalności serwisu :)
 		$this->useCase->__invoke(
 			'test@example.com',
 			'haslo'
