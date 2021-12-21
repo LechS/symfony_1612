8947c07dc3f9dc6d9becd33649d30fe9ed5323f0
Lech Sawon <lech.sawon@docplanner.com>
2021-12-21 07:46:16 +0100

    empty controller for adding post

--- src/Controller/Api/PostController.php
+++ src/Controller/Api/PostController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Api;
 
 use App\Entity\Post;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\SerializerInterface;
@@ -15,7 +16,25 @@ final class PostController
 	#[Route('/{id}', name: 'api_post_show', methods: ['GET'])]
 	public function show(Post $post, SerializerInterface $serializer): JsonResponse
 	{
-		$post = $serializer->serialize($post);
+		$post = $serializer->serialize($post, 'json');
 		return new JsonResponse(data: $post, json: true);
 	}
+
+
+
+	#[Route('', name: 'api_post_new', methods: ['POST'])]
+	public function new(Request $request): JsonResponse
+	{
+		dd($request->getContent());
+
+		return new JsonResponse([], 200);
+	}
+
+// @TODO list posts in api
+//	#[Route('/', name: 'api_post_show', methods: ['GET'])]
+//	public function index(Post $post, SerializerInterface $serializer): JsonResponse
+//	{
+//		$post = $serializer->serialize($post, 'json');
+//		return new JsonResponse(data: $post, json: true);
+//	}
 }
